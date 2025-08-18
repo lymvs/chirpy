5f379d982cf8e54b7a33ef65313e589dfdda4de9
lymvs <lysandros.mavropoulos@gmail.com>
2025-08-18 20:47:15 +0000

    added chirpy logo

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 )
 
-const port = "8080"
-
 func main() {
+	const port = "8080"
+	const filepathRoot = "."
+
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
