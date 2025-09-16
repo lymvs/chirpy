1278a3d1bcdd6f0700540b40766b80cda2adc12c
lymvs <lysandros.mavropoulos@gmail.com>
2025-09-16 21:25:09 +0000

    Add an endpoint handler

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ func main() {
 	const filepathRoot = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerEndpoint)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handlerEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
